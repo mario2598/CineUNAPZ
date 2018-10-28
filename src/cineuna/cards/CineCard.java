3151f868e2974c81c58dcd8fc4dcc6cbc104d397
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-10-27 19:37:41 -0600

    antes del merge

---
+++ src/cineuna/cards/CineCard.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cineuna.cards;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author robri
+ */
+public class CineCard extends Card{
+    private ImageView imagenCine;
+
+    @Override
+    public void initCard() {
+    }
+    
+    
+
+}
